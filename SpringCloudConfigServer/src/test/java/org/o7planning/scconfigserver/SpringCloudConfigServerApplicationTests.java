2a347675603470c3b72362d5c9c9e6c020fab82d
SriiLikhitha <116786719+SriiLikhitha@users.noreply.github.com>
2022-12-15 14:25:25 +0530

    Add files via upload

---
+++ SpringCloudConfigServer/src/test/java/org/o7planning/scconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+
+package org.o7planning.scconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
